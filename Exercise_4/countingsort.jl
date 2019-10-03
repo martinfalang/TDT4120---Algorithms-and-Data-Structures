273eece8bfef68ed8dd9b314d2c30f91397fb228
Martin Falang <falang.martin@gmail.com>
2019-10-03 13:22:19 +0200

    Cleaned up code

--- Exercise_4/countingsort.jl
+++ Exercise_4/countingsort.jl
@@ -41,7 +41,7 @@ end
 
 ### Tests ###
 
-printstyled("\n\n\n---------------\nKjører tester!!\n---------------\n"; color = :magenta)
+printstyled("\n\n\n---------------\nRunning tests!\n---------------\n"; color = :magenta)
 
 using Test
 @testset "Basic tests" begin
@@ -50,7 +50,3 @@ using Test
     @test countingsortletters(["ac", "bb", "ca"], 2) == ["ca", "bb", "ac"]
     @test countingsortletters(["ccc", "cba", "ca", "ab", "abc"], 2) == ["ca", "cba", "ab", "abc", "ccc"]
 end
-
-println("\nFungerte alt? Prøv å kjør koden i inginious!")
-println("Husk at disse testene ikke alltid sjekker alle edge-cases")
-println("---------------------------------------------------------\n\n")
